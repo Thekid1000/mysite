9af798fca3674da84e3ca8b6f71eba4344eea584
Thekid1000 <atif2007mahmud@gmail.com>
2022-04-17 11:30:25 +0000

    init

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import {Parallax,ParallaxLayer } from "@react-spring/parallax"
 export default function Home() {
   return (
     <>
-      <Head></Head>
-      <div className={styles.container}>
-        <div className={styles.main}>
+      <Head>
+        <title>MainPage</title>
+      </Head>
+      <div className={styles.main}>
+        <div className={styles.title}>
           Hello World
         </div>
       </div>
