19f5a3e26c09e947c074fb1ec1ca1da587c479c8
Thekid1000 <atif2007mahmud@gmail.com>
2022-04-17 11:11:43 +0000

    init

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,13 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.title}><div className={styles.main}>Hello World</div>
-    </div></div>
+    <>
+      <Head></Head>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          Hello World
+        </div>
+      </div>
+    </>
   );
 }
